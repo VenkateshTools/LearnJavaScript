639219154e4e4336139cf80b987e9122a6b0b497
vr <mandapetarv@gmail.com>
2022-11-22 21:50:36 +0530

    commitingFirstTime

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 console.log("start of the index.js");
 let count = 0;
 console.log(count);
+function increment(){
+    console.log("button is clicked");
+    count++;
+    console.log(count);
+    document.getElementById("count-el").innerText = count;
+}
 console.log("end of the index.js");
